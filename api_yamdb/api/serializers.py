c27564ab7db89fee0fe1340217bcc9263b3da8b8
Работа Работа <bogdan0957@yandex.ru>
2024-02-08 15:28:03 +0400

    Review v1.2

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from rest_framework import serializers, status
+from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
 from reviews.models import Category, Comments, Genre, Review, Title
