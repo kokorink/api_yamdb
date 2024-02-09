e61acdf1e157e454decb49955f4975d73616372e
kokorink <kokorin.konstantin@gmail.com>
2024-02-09 22:02:48 +0400

    w/o user/mail check in signup

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,7 +8,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
